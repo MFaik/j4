021fd5a4478a18b0cd8de719576e3590ce5418f8
Kenpar <erengrl54@gmail.com>
2021-06-05 11:46:42 +0300

    level system with transitions

--- Assets/Scripts/Level Manager/LevelManager.cs
+++ Assets/Scripts/Level Manager/LevelManager.cs
@@ -1,16 +1,29 @@
 ﻿using System;
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 public class LevelManager : MonoBehaviour
 {
+    public Animator animator;
+    public float transitionTime = 1.5f;
     public void LoadLevel(string levelName)
     {
-        SceneManager.LoadScene(levelName);
+        StartCoroutine(LoadLevelAnimation(levelName));
     }
 
     public void Finish(int nextLevel)
     {
         PlayerPrefs.SetInt("UnlockedLevel", nextLevel);
         SceneManager.LoadScene("level"+nextLevel);
+    }
+
+    IEnumerator LoadLevelAnimation(string levelName)
+    {
+        animator.SetTrigger("close");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelName);
     }
 }
