b1de73f4336d559e55fc85fb6f764104319010ca
Kenpar <erengrl54@gmail.com>
2021-06-05 11:21:00 +0300

    levelManager v1

---
+++ Assets/Scripts/Level Manager/LevelManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LevelManager : MonoBehaviour
+{
+    public void LoadLevel(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void Finish(int nextLevel)
+    {
+        PlayerPrefs.SetInt("UnlockedLevel", nextLevel);
+        SceneManager.LoadScene("level"+nextLevel);
+    }
+}
---
+++ Assets/Scripts/Level Manager/LevelSelector.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class LevelSelector : MonoBehaviour
+{
+    int UnlockedLevel;
+
+    public LevelManager manager;
+    public Button[] buttons;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UnlockedLevel = PlayerPrefs.GetInt("UnlockedLevel", 1);
+
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            buttons[i].interactable = false;
+        }
+
+        for (int i = 0; i < UnlockedLevel; i++)
+        {
+            buttons[i].interactable = true;
+        }
+        Debug.Log("Unlocked Level " + UnlockedLevel);
+    }
+
+    public void DeleteProgress()
+    {
+        UnlockedLevel = 1;
+        PlayerPrefs.SetInt("UnlockedLevel", 1);
+        manager.LoadLevel("levelSelector");
+    }
+
+}
