91a12556bf7722da1ad0cbcdc17f54c55e96a841
Kenpar <erengrl54@gmail.com>
2021-06-06 13:47:40 +0300

    credits + sound

---
+++ Assets/Scripts/Sound/MusicSource.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicSource : MonoBehaviour
+{
+    public static int volume = 50;
+
+    public static GameObject instance;
+
+    AudioSource audioSource;
+
+    private void Start() {
+        audioSource = GetComponent<AudioSource>();
+        if(instance)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = gameObject;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+    
+    void Update()
+    {
+        audioSource.volume = volume/100f;
+        if(!audioSource.isPlaying)
+        {
+            audioSource.Play();
+        }
+    }
+}
